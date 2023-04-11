24997b71a5b47a584de66b5a0b04e00906d84e0f
João Matheus <matheussabinovr7@gmail.com>
2023-04-10 23:27:14 -0300

    Criação método de excluir e editar de Estoque

--- src/page/Stock/index.js
+++ src/page/Stock/index.js
@@ -28,6 +28,8 @@ export default function Stock() {
   const [product, setProduct] = useState('');
   const [products, setProducts] = useState([]);
   const [stocks, setStocks] = useState([]);
+  const [id, setId] = useState('');
+  const [selector, setSelector] = useState(false);
 
   const listProducts = async () => {
     const resultProducts = await getProducts();
@@ -52,39 +54,93 @@ export default function Stock() {
     }
   };
 
+  const edit = (index) => {
+    const stock = { ...stocks };
+    setSelector(true);
+    setId(stock[index]._id);
+    setCode(stock[index].code);
+    setProduct(stock[index].product._id);
+    setAmount(stock[index].amount);
+    setType(stock[index].type);
+    setUnitOfMeasurement(stock[index].unitOfMeasurement);
+    setBatch(stock[index].batch);
+    setValidity(moment(stock[index].validity).format('YYYY-MM-DD'));
+    document.getElementById('products').value = stock[index].product.nome;
+    document.getElementById('unitOfMeasurement').value = stock[index].unitOfMeasurement;
+    document.getElementById('type').value = stock[index].type;
+    document.getElementById('validity').value = moment(stock[index].validity).format('YYYY-MM-DD');
+  };
+
+  const clear = () => {
+    setId('');
+    setSelector(false);
+    setCode('');
+    setAmount(0);
+    setProduct('');
+    setUnitOfMeasurement('');
+    setValidity(new Date());
+    setType('');
+    setBatch('');
+    document.getElementById('products').value = '...';
+    document.getElementById('unitOfMeasurement').value = '...';
+    document.getElementById('type').value = '...';
+  };
+
   const save = async () => {
-    if (code === '') {
-      toast.error('O código do produto deve ser informado !');
-      return;
-    }
+    if (selector) {
+      await axios
+        .post(`/stock/${id}`, { code, product: product._id, unitOfMeasurement, amount, validity, type, batch })
+        .then(() => toast.success('Editado com sucesso !'))
+        .catch((error) => {
+          toast.error(error.response.data.message);
+        });
 
-    if (product === '') {
-      toast.error('O produto deve ser informado !');
-      return;
-    }
+      clear();
+      listStocks();
+    } else {
+      if (code === '') {
+        toast.error('O código do produto deve ser informado !');
+        return;
+      }
 
-    if (unitOfMeasurement === '') {
-      toast.error('A unidade de medida deve ser informada !');
-      return;
-    }
+      if (product === '') {
+        toast.error('O produto deve ser informado !');
+        return;
+      }
 
-    if (amount === 0 && isNumber(amount)) {
-      toast.error('A quantidade deve ser informada ou precisa ser um número !');
-      return;
-    }
+      if (unitOfMeasurement === '') {
+        toast.error('A unidade de medida deve ser informada !');
+        return;
+      }
 
-    if (code === '') {
-      toast.error('O código do produto deve ser informado');
-      return;
-    }
+      if (amount === 0 && isNumber(amount)) {
+        toast.error('A quantidade deve ser informada ou precisa ser um número !');
+        return;
+      }
 
+      if (code === '') {
+        toast.error('O código do produto deve ser informado');
+        return;
+      }
+
+      await axios
+        .post('/stock/new', { code, product: product._id, unitOfMeasurement, amount, validity, type, batch })
+        .then(() => toast.success('Salvo com sucesso !'))
+        .catch((error) => {
+          toast.error(error.response.data.message);
+        });
+
+      clear();
+      listStocks();
+    }
+  };
+
+  const deleteStock = async (id) => {
     await axios
-      .post('/stock/new', { code, product: product._id, unitOfMeasurement, amount, validity, type, batch })
-      .then(() => toast.success('Salvo com sucesso !'))
-      .catch((error) => {
-        toast.error(error.response.data.message);
-      });
-
+      .delete(`/stock/${id}`)
+      .then(() => toast.success('Excluído com sucesso !'))
+      .catch((error) => toast.error(error.response.data.message));
+    clear();
     listStocks();
   };
 
@@ -98,9 +154,9 @@ export default function Stock() {
         <Card.Body>
           <Form>
             <Row className="m-3">
-              <Form.Group as={Col} md="3" controlId="validationCustom01">
+              <Form.Group as={Col} md="3" controlId="code">
                 <Form.Label>Código</Form.Label>
-                <Form.Control type="text" placeholder="Nome do Produto" value={code} onChange={(e) => setCode(e.target.value)} />
+                <Form.Control type="text" iplaceholder="Nome do Produto" value={code} onChange={(e) => setCode(e.target.value)} />
               </Form.Group>
               <Form.Group as={Col} md="4" controlId="products">
                 <Form.Label>Produtos</Form.Label>
@@ -120,13 +176,13 @@ export default function Stock() {
                   <option value="UND">Unidade</option>
                 </Form.Select>
               </Form.Group>
-              <Form.Group as={Col} md="2">
+              <Form.Group as={Col} md="2" controlId="amount">
                 <Form.Label>Quantidade</Form.Label>
                 <Form.Control type="number" value={amount} onChange={(e) => setAmount(parseFloat(e.target.value))} />
               </Form.Group>
             </Row>
             <Row className="m-3">
-              <Form.Group as={Col} md="3" controlId="validationCustom01">
+              <Form.Group as={Col} md="3" controlId="validity">
                 <Form.Label>Validade</Form.Label>
                 <Form.Control type="date" placeholder="Nome do Produto" value={validity} onChange={(e) => setValidity(e.target.value)} />
               </Form.Group>
@@ -140,7 +196,7 @@ export default function Stock() {
 
               <Form.Group as={Col} md="3">
                 <Form.Label>Lote</Form.Label>
-                <Form.Control type="text" value={batch} onChange={(e) => setBatch(e.target.value)} />
+                <Form.Control type="text" id="batch" value={batch} onChange={(e) => setBatch(e.target.value)} />
               </Form.Group>
             </Row>
             <Row className="m-3">
@@ -168,7 +224,7 @@ export default function Stock() {
                 </tr>
               </thead>
               <tbody>
-                {stocks.map((skt) => (
+                {stocks.map((skt, index) => (
                   // eslint-disable-next-line no-underscore-dangle
                   <tr key={skt._id}>
                     <td>{skt.code}</td>
@@ -179,10 +235,10 @@ export default function Stock() {
                     <td>{skt.type}</td>
                     <td>{skt.batch}</td>
                     <td>
-                      <CiEdit className="icon-edit" size={23} />
+                      <CiEdit className="icon-edit" size={23} onClick={() => edit(index)} />
                     </td>
                     <td>
-                      <TiDeleteOutline className="icon-delete" size={23} />
+                      <TiDeleteOutline className="icon-delete" size={23} onClick={() => deleteStock(skt._id)} />
                     </td>
                   </tr>
                 ))}
